a148580d708bb5bf80573838a0c05fd6a295cff3
Michal Przysucha <michal.przysucha@adform.com>
2019-12-17 12:00:12 +0100

    second

--- src/main/java/first/DL4JLogReg.java
+++ src/main/java/first/DL4JLogReg.java
@@ -1,19 +1,31 @@
 package first;
 
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.SplitTestAndTrain;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.io.ClassPathResource;
 import org.nd4j.linalg.learning.config.Sgd;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.io.IOException;
 
 public class DL4JLogReg {
 
     public static void main(String[] args) {
 
-        int numOfFeatures = (int) Math.pow(2, 29);
+//        int numOfFeatures = (int) Math.pow(2, 29);
+        int numOfFeatures = 4;
 
         NeuralNetConfiguration.Builder builder = new NeuralNetConfiguration.Builder();
 
@@ -30,7 +42,31 @@ public class DL4JLogReg {
         MultiLayerNetwork network = new MultiLayerNetwork(conf);
         network.init();
 
-        network.fit();
+        try (RecordReader recordReader = new CSVRecordReader(0, ',')) {
+            recordReader.initialize(new FileSplit(new ClassPathResource("iris.txt").getFile()));
+
+
+            DataSetIterator iterator = new RecordReaderDataSetIterator(
+                    recordReader, 6, 4, 1);
+            DataSet allData = iterator.next();
+            allData.shuffle(42);
+
+
+            SplitTestAndTrain testAndTrain = allData.splitTestAndTrain(0.65);
+            DataSet trainingData = testAndTrain.getTrain();
+            DataSet testData = testAndTrain.getTest();
+
+            network.fit(trainingData);
+            INDArray params = network.params();
+            System.out.println(params);
+            System.out.println(params.shapeInfoToString());
+
+
+            // …
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
 
 
 
